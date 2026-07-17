09e0c72f082a208cd36cd4a23b5d645e870c1da4
bea <164930551+beatxz@users.noreply.github.com>
2026-07-17 11:17:20 -0300

    Primeiro commit

---
+++ src/main/java/com/dev/gestorNF/GestorNfApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gestorNF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorNfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorNfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/gestorNF/GestorNfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gestorNF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorNfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
