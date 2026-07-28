3d2c2ebed2ab97cd423358f753b5ff72fb2fd70f
bea <164930551+beatxz@users.noreply.github.com>
2026-07-28 18:12:34 -0300

    Adicionado Converter

--- src/main/java/com/dev/gestorNF/business/mapper/UsuarioConverter.java
+++ src/main/java/com/dev/gestorNF/business/mapper/UsuarioConverter.java
@@ -1,8 +1,29 @@
 package com.dev.gestorNF.business.mapper;
 
+import com.dev.gestorNF.business.dto.in.UsuarioDTORequest;
+import com.dev.gestorNF.business.dto.out.UsuarioDTOResponse;
+import com.dev.gestorNF.infrastructure.entity.out.UsuarioEntity;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Converter {
-    
+public class UsuarioConverter {
+
+    public UsuarioEntity paraUsuarioEntity(UsuarioDTORequest usuarioDTORequest){
+        return UsuarioEntity.builder()
+                .email(usuarioDTORequest.getEmail())
+                .nome(usuarioDTORequest.getNome())
+                .senha(usuarioDTORequest.getSenha())
+                .build();
+    }
+
+    public UsuarioDTOResponse paraUsuarioDTOResponse (UsuarioEntity entity){
+        return UsuarioDTOResponse.builder()
+                .id(entity.getId())
+                .email(entity.getEmail())
+                .nome(entity.getNome())
+                .senha(entity.getSenha())
+                .build();
+    }
+
+
 }
