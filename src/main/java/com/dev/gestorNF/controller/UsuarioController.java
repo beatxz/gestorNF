37e1ff1b6de6e4f80be1eda521a0fcc4e456b30f
bea <164930551+beatxz@users.noreply.github.com>
2026-08-14 16:38:29 -0300

    Swagger adicionado a aplicação

--- src/main/java/com/dev/gestorNF/controller/UsuarioController.java
+++ src/main/java/com/dev/gestorNF/controller/UsuarioController.java
@@ -4,7 +4,10 @@ import com.dev.gestorNF.business.UsuarioService;
 import com.dev.gestorNF.business.dto.in.UsuarioDTORequest;
 import com.dev.gestorNF.business.dto.out.UsuarioDTOResponse;
 import com.dev.gestorNF.infrastructure.security.SecurityConfig;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,24 +16,41 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuario")
+@Tag(name = ("Usuario"), description = "Cadastro e login do usuário")
 @SecurityRequirement(name = SecurityConfig.SECURITY_SCHEME)
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @Operation(summary = "Salvar usuário", description = "Cria um novo usuário")
+    @ApiResponse(responseCode = "200", description = "usuário salvo com sucesso")
+    @ApiResponse(responseCode = "400", description = "usuário já cadastrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @PostMapping
     public ResponseEntity<UsuarioDTOResponse> salvarUsuario(@RequestBody UsuarioDTORequest usuarioDTORequest) {
         return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTORequest));
     }
+    @Operation(summary = "Login usuário", description = "Faz o login do usuário")
+    @ApiResponse(responseCode = "200", description = "Login efetuado sucesso")
+    @ApiResponse(responseCode = "400", description = "Usuário não cadastrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @PostMapping("/login")
     public ResponseEntity<String>login(@RequestBody UsuarioDTORequest usuarioDTORequest){
         return ResponseEntity.ok(usuarioService.autenticarUsuario(usuarioDTORequest));
     }
+    @Operation(summary = "Deletar usuário", description = "Deleta um usuário por email")
+    @ApiResponse(responseCode = "200", description = "Usuário deletado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Usuário não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @DeleteMapping("/{email}")
     public ResponseEntity<Void>deletarUsuarioEmail(@PathVariable String email){
         usuarioService.deletaUsuarioEmail(email);
          return ResponseEntity.ok().build();
     }
+    @Operation(summary = "Buscar usuário", description = "Busca um usuário por Email")
+    @ApiResponse(responseCode = "200", description = "usuário encontrado com sucesso")
+    @ApiResponse(responseCode = "400", description = "usuário não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @GetMapping
     public ResponseEntity<UsuarioDTOResponse>buscarUsuarioEmail(@RequestParam("email") String email){
        return ResponseEntity.ok(usuarioService.buscaUsuarioEmail(email));
--- src/main/java/com/dev/gestorNF/controller/VendedorController.java
+++ src/main/java/com/dev/gestorNF/controller/VendedorController.java
@@ -3,6 +3,9 @@ package com.dev.gestorNF.controller;
 import com.dev.gestorNF.business.VendedorService;
 import com.dev.gestorNF.business.dto.in.VendedorDTORequest;
 import com.dev.gestorNF.business.dto.out.VendedorDTOResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,28 +13,45 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/vendedor")
+@Tag(name = "vendedor", description = "Cadastro e configuração de vendedor")
 
 public class VendedorController {
 
     private final VendedorService vendedorService;
 
+    @Operation(summary = "Cadastrar vendedor", description = "Cadastro do vendedor")
+    @ApiResponse(responseCode = "200" , description = "Cadastro realizado com sucesso")
+    @ApiResponse(responseCode = "400" , description = "Vendedor já cadastrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @PostMapping
-    public ResponseEntity<VendedorDTOResponse> cadastrarVendedor(@RequestHeader("Authorization") String token,
+    public ResponseEntity<VendedorDTOResponse> cadastrarVendedor(@RequestHeader(name = "Authorization",required = false) String token,
                                                                 @RequestBody VendedorDTORequest vendedorDTORequest){
         return ResponseEntity.ok(vendedorService.cadastroVendedor(token,vendedorDTORequest));
     }
+    @Operation(summary = "Deletar vendedor", description = "Deleta vendedor")
+    @ApiResponse(responseCode = "200" , description = "Vendedor deletado com sucesso")
+    @ApiResponse(responseCode = "400" , description = "Vendedor não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarVendedor(@PathVariable("id") Long idVendedor){
         vendedorService.deletaVendedor(idVendedor);
         return ResponseEntity.ok().build();
     }
+    @Operation(summary = "Buscar vendedor", description = "Buscar vendedor por Id")
+    @ApiResponse(responseCode = "200" , description = "Vendedor encontrado com sucesso")
+    @ApiResponse(responseCode = "400" , description = "Vendedor não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @GetMapping("/{id}")
-    public ResponseEntity<VendedorDTOResponse> buscaVendedorId(@RequestHeader("Authorization") String token,
+    public ResponseEntity<VendedorDTOResponse> buscaVendedorId(@RequestHeader(name = "Authorization",required = false) String token,
                                                                @RequestParam ("id") Long idVendedor ){
         return ResponseEntity.ok(vendedorService.buscarVendedorPorId(token,idVendedor));
     }
+    @Operation(summary = "Alterar comissão", description = "Altera comissão do vendedor")
+    @ApiResponse(responseCode = "200" , description = "Comissão alterada com sucesso")
+    @ApiResponse(responseCode = "400" , description = "Vendedor não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     @PatchMapping
-    public ResponseEntity<VendedorDTOResponse> updateComissao(@RequestHeader("Authorization") String token,
+    public ResponseEntity<VendedorDTOResponse> updateComissao(@RequestHeader(name = "Authorization",required = false) String token,
                                                               @RequestParam("id") Long idVendedor,
                                                               @RequestBody Double comissao){
         return ResponseEntity.ok(vendedorService.updateComissao(token, idVendedor,comissao));
