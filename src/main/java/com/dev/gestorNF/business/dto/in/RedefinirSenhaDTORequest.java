bc26613aa5f30513b5daaa2122b224566677c37e
bea <164930551+beatxz@users.noreply.github.com>
2026-08-31 15:20:11 -0300

    Atualização fix: configura rotas SPA na Vercel

--- src/main/java/com/dev/gestorNF/business/dto/in/RedefinirSenhaDTORequest.java
+++ src/main/java/com/dev/gestorNF/business/dto/in/RedefinirSenhaDTORequest.java
@@ -1,5 +1,7 @@
 package com.dev.gestorNF.business.dto.in;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,13 @@ import lombok.Setter;
 @Setter
 public class RedefinirSenhaDTORequest {
 
+    @NotBlank(message = "Token de recuperação é obrigatório")
     private String token;
+
+    @NotBlank(message = "A nova senha é obrigatória")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9\\s]).{8,}$",
+            message = "A senha deve ter pelo menos 8 caracteres, incluindo letra maiúscula, letra minúscula, número e caractere especial"
+    )
     private String novaSenha;
 }
